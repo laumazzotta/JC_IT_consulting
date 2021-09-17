c90f91af35177681b72916aaa23d289c5d388c5b
Lali Mazzotta <lau.mazzotta@gmail.com>
2021-09-16 21:41:04 -0300

    commit web

---
+++ scripts.js
@@ -0,0 +1,98 @@
+jQuery(function($) {
+
+    $(window).scroll(function(a, b, c) {
+        const offset = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+        $('body').css('--scroll', offset);
+    });
+
+    // $('.items-qs').addClass('animate__slideInUp');
+    $('.hero-tittle > h1').addClass('slideInDown');
+
+    // window.addEventListener('scroll', () => {
+    //     let offset = document.body.offsetHeight - window.innerHeight;
+    //     let scroll = (offset > 0) ? window.pageYOffset : 0;
+    //     document.body.style.setProperty('--scroll', scroll);
+    //   }, false);
+
+    $('#openNav').on('click', function() {
+        $('#myNav').css("height", "100%");
+    });
+    
+    $('#closeNav').on('click', function() {
+        $('#myNav').css("height", "0%");
+    });
+
+    $('.nav-item, .dropdown-item').on('click', function() {
+        $('#myNav').css("height", "0%");
+    });
+
+    // PLANEAMIENTO
+    $('#open-btn-1').on('click', function() {
+        $('.planeamiento').addClass("planeamiento-open");
+        $('.overlay').addClass("overlay-open");
+    });
+    
+    $('#close-btn-1').on('click', function() {
+        $('.planeamiento').removeClass("planeamiento-open");
+        $('.overlay').removeClass("overlay-open");
+    });
+
+    // DIRECCION
+    $('#open-btn-2').on('click', function() {
+        $('.direccion').addClass("direccion-open");
+        $('.overlay-2').addClass("overlay-open");
+    });
+    
+    $('#close-btn-2').on('click', function() {
+        $('.direccion').removeClass("direccion-open");
+        $('.overlay-2').removeClass("overlay-open");
+    });
+
+    // REPORTING
+    $('#open-btn-3').on('click', function() {
+        $('.reporting').addClass("planeamiento-open");
+        $('.overlay-3').addClass("overlay-open");
+    });
+    
+    $('#close-btn-3').on('click', function() {
+        $('.reporting').removeClass("planeamiento-open");
+        $('.overlay-3').removeClass("overlay-open");
+    });
+
+    // PROCESOS IT
+    $('#open-btn-4').on('click', function() {
+        $('.procesosIT').addClass("direccion-open");
+        $('.overlay-4').addClass("overlay-open");
+    });
+    
+    $('#close-btn-4').on('click', function() {
+        $('.procesosIT').removeClass("direccion-open");
+        $('.overlay-4').removeClass("overlay-open");
+    });
+
+    // STAFFING
+    $('#open-btn-5').on('click', function() {
+        $('.staffing').addClass("planeamiento-open");
+        $('.overlay-5').addClass("overlay-open");
+    });
+    
+    $('#close-btn-5').on('click', function() {
+        $('.staffing').removeClass("planeamiento-open");
+        $('.overlay-5').removeClass("overlay-open");
+    });
+
+
+    // $('#navLinks').on('click', function() {
+    //     $('#myNav').css("height", "0%");
+    // });
+   
+    //   var $textoHaras = $('.texto-haras'); 
+    //   var $plusButton = $('.plusButton'); 
+
+    //   $plusButton.click(function(){
+    //     $(this).removeClass('d-block');
+    //     $(this).addClass('d-none');
+    //     $textoHaras.removeClass('pb-5');
+    // });
+
+});
